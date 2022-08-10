036bfcb578a6f225667bfb578bf67f21966c16bb
Daemon19 <komanglangendria04@gmail.com>
2022-08-10 20:30:55 +0800

    create initial layout

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
 
 const indexRouter = require('./routes/index')
 
@@ -6,6 +8,10 @@ const PORT = process.env.PORT ?? 3000
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(indexRouter)
+app.use(expressLayouts)
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`))
